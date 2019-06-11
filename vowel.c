c8c2be7928a761319533eccf41d39ec386ca645f
sreeramsuresh28 <51689270+sreeramsuresh28@users.noreply.github.com>
2019-06-11 10:15:42 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char num;
+    scanf("%c",&num);
+    if(isalpha(num) == 0)   {
+        printf("invalid");
+        return 0;
+    }
+
+    num = tolower(num);
+    if(num=='a' || num=='e' || num=='i' || num=='o' || num=='u')
+    {
+        printf("Vowel");
+    }
+    else
+    {
+        {
+            printf("Consonant");
+        }
+    }
+    return 0;
+}
