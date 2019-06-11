ca0dd0eecc1c9de6671c3d522e20d646c2736f26
sreeramsuresh28 <51689270+sreeramsuresh28@users.noreply.github.com>
2019-06-11 12:34:11 +0530

    Add files via upload

---
+++ first_rep.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int lookup[100000] = { 0 };
+    int n;
+    scanf("%d", &n);
+    int a;
+    int hasRep = 0;
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a);
+        lookup[a]++;
+        if(lookup[a] == 2)      {
+            printf("%d ", a);
+            hasRep = 1;
+            break;
+        }
+    }
+    if(!hasRep) printf("no unique");
+
+    return 0;
+}
