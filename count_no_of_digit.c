0980cf6d43dfc9c676d6242a2510c33dc235ca09
sreeramsuresh28 <51689270+sreeramsuresh28@users.noreply.github.com>
2019-06-11 19:30:04 +0530

    Update count_no_of_digit.c

--- count_no_of_digit.c
+++ count_no_of_digit.c
@@ -1,14 +1,15 @@
+
 #include<stdio.h>
 int main()
 {
     long long int num,count=0,rem;
-    scanf("%d",&num);
+    scanf("%lld",&num);
     while(num)
     {
         rem=num%10;
         count++;
         num=num/10;
     }
-    printf("%d",count);
+    printf("%lld",count);
     return 0;
 }
