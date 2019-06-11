a7e897a2820d01cba2c16aff1f2ceb990af5f2cf
sreeramsuresh28 <51689270+sreeramsuresh28@users.noreply.github.com>
2019-06-11 10:01:18 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("Even");
+    }
+    else if(num%2!=0)
+    {
+        printf("Odd");
+    }
+    else
+        {
+            printf("invalid");
+        }
+        return 0;
+    
+}
