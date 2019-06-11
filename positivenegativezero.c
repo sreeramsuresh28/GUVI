ddc99b2410995b1a3f04aa23504399aa1d53c2c4
sreeramsuresh28 <51689270+sreeramsuresh28@users.noreply.github.com>
2019-06-11 09:16:26 +0530

    Add files via upload

---
+++ positivenegativezero.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+
+{
+    int num;
+    scanf("%d",&num);
+    if(num==0)
+    {
+        printf("Zero");
+    }
+    else if(num>0)
+    {
+        printf("Positive");
+    }
+    else if(num<0)
+    {
+        printf("Negative");
+    }
+
+    return 0;
+}
