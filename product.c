3c41648271cd7e6de59bc7ba2766df8e6354928f
sreeramsuresh28 <51689270+sreeramsuresh28@users.noreply.github.com>
2019-06-11 13:47:43 +0530

    Add files via upload

---
+++ product.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,prod=1,i;
+ 
+    scanf("%d",&n);
+    int arr[n];
+   for(i=0;i<n;i++)
+{
+    scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+    prod=prod*arr[i];
+}
+
+printf("%d",prod);
+
+
+return 0;
+}
