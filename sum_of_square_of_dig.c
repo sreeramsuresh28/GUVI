088ae116acc9c3afa747ce20b1514ed6c7b2d89f
sreeramsuresh28 <51689270+sreeramsuresh28@users.noreply.github.com>
2019-06-12 17:23:49 +0530

    Update sum_of_square_of_dig.c

--- sum_of_square_of_dig.c
+++ sum_of_square_of_dig.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int num,dig=0,rem;
-    scanf("%d",&num);
+   long long int num,dig=0,rem;
+    scanf("%lld",&num);
     while(num)
     {
         rem=num%10;
         dig=dig+(rem*rem);
         num=num/10;
     }
-    printf("%d",dig);
+    printf("%lld",dig);
     return 0;
 }
